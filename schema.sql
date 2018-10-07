1056d330994f54fcc778cce0d203ca403102d32e
Steven Berk <steven.e.berk@gmail.com>
2018-10-07 11:40:42 -0400

    changed integer data type to numeric

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     firstname character varying(200) NOT NULL,
     lastname character varying(200) NOT NULL,
     email character varying(255) UNIQUE NOT NULL,
-    password character NOT NULL,
+    password character varying(50) NOT NULL,
 
 
 );
@@ -11,13 +11,13 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
     postid serial UNIQUE NOT NULL,
-    amount integer NOT NULL,
+    amount numeric NOT NULL,
     currency character varying(5) NOT NULL,
     location character varying(50) NOT NULL,
     notes character varying(255),
-    valueInUSD integer NOT NULL,
+    valueInUSD numeric NOT NULL,
     selleremail character varying(255) NOT NULL,
     sellername character varying(200) NOT NULL,
-    sellerid integer NOT NULL
+    sellerid numeric NOT NULL
     
 );
